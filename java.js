fc4ef10417ed93d4a842545fac970d19e6e9db78
Laura <92340827+lauralima6@users.noreply.github.com>
2022-08-23 02:15:10 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,75 @@
+
+function subir1(){
+  
+    window.scrollTo({
+        top:0,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function subir2(){
+  
+    window.scrollTo({
+        top:645,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function subir3(){
+    window.scrollTo({
+        top: 1280,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function subir4(){
+  
+    window.scrollTo({
+        top: 1920,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function subir5(){
+  
+    window.scrollTo({
+        top:645,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function descer(){
+    window.scrollTo({
+        top:645,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function descer1(){
+    window.scrollTo({
+        top:1280,
+        left:0,
+        behavior: 'smooth'
+    });
+}
+function descer2(){
+    window.scrollTo({
+        top: 1920,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+function descer3(){
+    window.scrollTo({
+        top: 5160,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+function descer4(){
+    window.scrollTo({
+        top: 10320,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
